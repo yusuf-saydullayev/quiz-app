474a83933ecda87205baebf910f90aed59e3a268
Yusuf <yusufsaydullayev@gmail.com>
2024-01-19 16:16:27 +0500

    data grid

--- src/routes/score/score.page.tsx
+++ src/routes/score/score.page.tsx
@@ -1,7 +1,14 @@
 import { Heading } from "~/components/typography/Heading";
+import { Text } from "~/components/typography/Text";
 import { Button } from "~/components/ui/Button";
+import { HotTable } from "@handsontable/react";
+import "handsontable/dist/handsontable.full.min.css";
+import { registerAllModules } from "handsontable/registry";
+import { Container } from "~/components/ui/Container";
 import { cn } from "~/libs/cn.lib";
 import { useAppSelector } from "~/store/hooks";
+
+registerAllModules();
 
 export const ScorePage = () => {
 	const score = useAppSelector((state) => state.userAnswers.score);
@@ -28,6 +35,158 @@ export const ScorePage = () => {
 					</div>
 				</article>
 			))}
+			<Container className="py-10 flex justify-center flex-col items-center">
+				<Heading className="my-6">Data grid</Heading>
+				<Text>I don't want to not configure and customaize</Text>
+				<HotTable
+					data={[
+						{
+							brand: "Jetpulse",
+							model: "Racing Socks",
+							price: 30,
+							sellDate: "Oct 11, 2023",
+							sellTime: "01:23 AM",
+						},
+						{
+							brand: "Gigabox",
+							model: "HL Mountain Frame",
+							price: 1890.9,
+							sellDate: "May 3, 2023",
+							sellTime: "11:27 AM",
+						},
+						{
+							brand: "Camido",
+							model: "Cycling Cap",
+							price: 130.1,
+							sellDate: "Mar 27, 2023",
+							sellTime: "03:17 AM",
+						},
+						{
+							brand: "Chatterpoint",
+							model: "Road Tire Tube",
+							price: 59,
+							sellDate: "Aug 28, 2023",
+							sellTime: "08:01 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Eidel",
+							model: "HL Road Tire",
+							price: 279.99,
+							sellDate: "Oct 2, 2023",
+							sellTime: "13:23 AM",
+						},
+						{
+							brand: "Camido",
+							model: "Cycling Cap",
+							price: 130.1,
+							sellDate: "Mar 27, 2023",
+							sellTime: "03:17 AM",
+						},
+						{
+							brand: "Camido",
+							model: "Cycling Cap",
+							price: 130.1,
+							sellDate: "Mar 27, 2023",
+							sellTime: "03:17 AM",
+						},
+						{
+							brand: "Camido",
+							model: "Cycling Cap",
+							price: 130.1,
+							sellDate: "Mar 27, 2023",
+							sellTime: "03:17 AM",
+						},
+					]}
+					columns={[
+						{
+							title: "name",
+							type: "text",
+							data: "brand",
+						},
+						{
+							title: "Model",
+							type: "text",
+							data: "model",
+						},
+						{
+							title: "Price",
+							type: "numeric",
+							data: "price",
+							numericFormat: {
+								pattern: "$ 0,0.00",
+								culture: "en-US",
+							},
+						},
+						{
+							title: "Date",
+							type: "date",
+							data: "sellDate",
+							dateFormat: "MMM D, YYYY",
+							correctFormat: true,
+							className: "htRight",
+						},
+						{
+							title: "Time",
+							type: "time",
+							data: "sellTime",
+							timeFormat: "hh:mm A",
+							className: "htRight",
+						},
+					]}
+					colWidths={100}
+					width="30%"
+					height={320}
+					rowHeaders={true}
+					colHeaders={true}
+					columnSorting={true}
+					fixedColumnsStart={1}
+					autoWrapRow={true}
+					autoWrapCol={true}
+					licenseKey="non-commercial-and-evaluation"
+				/>
+			</Container>
 		</section>
 	);
 };
